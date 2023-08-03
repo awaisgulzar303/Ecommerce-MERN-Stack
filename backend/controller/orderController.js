9f3104781938b7212114cd47a61b5841f5a820dd
awaisgulzar303 <47609855+awaisgulzar303@users.noreply.github.com>
2023-08-04 00:31:25 +0500

    CREATE ORDER API AND MANAGE ORDER APIS

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -32,3 +32,60 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     order,
   });
 });
+
+//get single order
+exports.getSingleOrderDetails = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "User",
+    "name email"
+  );
+
+  if (!order) {
+    return new ErrorHandler("Order not Found", 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+//get single order details by id
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+  const order = await Order.find({ User: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//get all orders --Admin
+exports.getAllOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find();
+
+  totalAmount = 0;
+  orders.forEach((order) => (totalAmount = totalAmount + order.itemsPrice));
+  res.status(200).json({
+    success: true,
+
+    totalAmount,
+  });
+});
+
+//update status of an order
+exports.updateOrderStatus = catchAsyncError(async (req, res, next) => {
+  let order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(new ErrorHandler("order with this ID not found", 404));
+  }
+  order = await Order.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,26 @@
 const express = require("express");
-const { newOrder } = require("../controller/orderController");
+const {
+  newOrder,
+  getSingleOrderDetails,
+  myOrders,
+  getAllOrders,
+  updateOrderStatus,
+} = require("../controller/orderController");
 const router = express.Router();
 const { authorizeRole, isAuthenticatedUser } = require("../middleware/auth");
 
 router.route("/newOrder").post(isAuthenticatedUser, newOrder);
+router
+  .route("/single/order/:id")
+  .get(isAuthenticatedUser, getSingleOrderDetails);
 
+router.route("/myOrder").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/myCurrentOrders")
+  .get(isAuthenticatedUser, authorizeRole("admin"), getAllOrders);
+
+router
+  .route("/update/Orderstatus/:id")
+  .post(isAuthenticatedUser, authorizeRole("admin"), updateOrderStatus);
 module.exports = router;
