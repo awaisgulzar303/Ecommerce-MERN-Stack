b744db9e4cd6bf305032ace7285c443427445c32
awaisgulzar303 <47609855+awaisgulzar303@users.noreply.github.com>
2023-08-22 04:19:10 +0500

    add packages for frontend and do set up of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import React from "react";
 import "./App.css";
-import Header from "./components/layout/Header.js";
+import Header from "./components/layout/Header/Header.js";
+import webFont from "webfontloader";
+import Footer from "./components/layout/Footer/Footer";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  React.useEffect(() => {
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  });
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
